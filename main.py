21d3e98a1d0d96d6dbdfed09dd3352e6692fbc2c
rjowell20007 <44178512+rjowell@users.noreply.github.com>
2022-04-26 19:16:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame
+
+#Initiate pygame
+pygame.init()
+
+#Sets up the game display
+window = pygame.display.set_mode((600,400))
+pygame.display.set_caption("Mr. Russ Cool Game")
+
+#defines our various color values
+Yellow = (255,255,0)
+White = (255,255,255)
+myColor = (50,100,150)
+randomColor = (60,80,120)
+bgColor = (200,10,60)
+'''
+(100,50,200)
+
+(10,200,70)
+
+
+R ed
+G reen
+B lue
+
+C yan
+M agenta
+Y ellow
+K Black
+
+'''
